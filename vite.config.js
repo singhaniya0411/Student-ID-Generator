48ff5fefd86a5a514136e5e58b4771337eb4697b
Vishal Singhaniya <vi.singhaniya2956@gmail.com>
2025-04-11 23:54:36 +0500

    Updates

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,8 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
   },
+  allowedHosts: [
+    '2351a5a4-febe-4305-8bc5-4b896e85632d-00-wtatve7a7481.sisko.replit.dev',
+  ],
   plugins: [react(), tailwindcss()],
 })
